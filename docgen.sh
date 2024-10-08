d21c4c1bfbb1e4078a788607ceb800ef05f3890a
Arindam Karmakar <arindamk410@gmail.com>
2024-10-09 01:13:02 +0530

    DocGen Fixes

--- docgen.sh
+++ docgen.sh
@@ -8,5 +8,4 @@ rm -rf $opath
 swift package --allow-writing-to-directory $opath \
  generate-documentation --disable-indexing \
   --output-path $opath \
-   --transform-for-static-hosting \
-    --hosting-base-path $bpath
+   --hosting-base-path $bpath
