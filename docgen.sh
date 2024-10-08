e947737db1f650b7f18000be45d21ea92aa85685
Arindam Karmakar <arindamk410@gmail.com>
2024-10-09 01:11:10 +0530

    DocGen Fixes

--- docgen.sh
+++ docgen.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 opath="./docs/"
-bpath="./docs/"
+bpath="SwiftNP"
 
 rm -rf $opath
 
 swift package --allow-writing-to-directory $opath \
  generate-documentation --disable-indexing \
   --output-path $opath \
-   --transform-for-static-hosting
-    # --hosting-base-path $bpath
+   --transform-for-static-hosting \
+    --hosting-base-path $bpath
